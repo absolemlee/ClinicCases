4a77b2aff34ea3202524efbcf6b1cf30597934dc
Absolem Lee <83130100+absolemlee@users.noreply.github.com>
2025-04-07 06:46:50 +0000

    Update keep_alive.php

--- lib/php/keep_alive.php
+++ lib/php/keep_alive.php
@@ -1,2 +1,11 @@
-error_log("Session ID: " . session_id());
-error_log("Session Data: " . print_r($_SESSION, true));
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Mostrar el ID de sesión y su contenido para depuración
+header("Content-Type: text/plain");
+
+echo "Session ID: " . session_id() . "\n";
+echo "Session data:\n";
+print_r($_SESSION);
