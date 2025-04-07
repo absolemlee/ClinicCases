b51dbcb240d3ac2108cea254ef4430341b9c5fff
Absolem Lee <83130100+absolemlee@users.noreply.github.com>
2025-04-07 06:58:12 +0000

    Update keep_alive.php

--- lib/php/auth/keep_alive.php
+++ lib/php/auth/keep_alive.php
@@ -1,11 +1,20 @@
 <?php
+header("Content-Type: text/plain");
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// Mostrar el ID de sesión y su contenido para depuración
-header("Content-Type: text/plain");
-
 echo "Session ID: " . session_id() . "\n";
-echo "Session data:\n";
+echo "Cookies:\n";
+print_r($_COOKIE);
+
+echo "Session:\n";
 print_r($_SESSION);
+
+if (!isset($_SESSION['cc_session_id'])) {
+    header("HTTP/1.0 401 Not Authorized");
+    echo "\ncc_session_id not set.\n";
+} else {
+    echo "OK\n";
+}
